3e33bf520efc5b0badfc75d38831f6e8111674c0
RaulCruzOrtega <raul_cruz_ortega@outlook.es>
2023-04-11 20:56:18 +0100

    First commit ngFor and @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona("Maria", "Montes de Oca"), 
+    new Persona("Laura", "Gorda")]
+  nombreInput = "";
+  apellidoInput = "";
+
+  agregarPersona(){
+    let persona = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
